c560c6a59dee7f90f41777dc119fe1671138b6ae
Android Dev <aamirnasa97@gmail.com>
2024-01-18 02:24:46 +0530

    button section is added

--- app/src/main/java/com/aamirashraf/instagramui_compose/ProfileScreen.kt
+++ app/src/main/java/com/aamirashraf/instagramui_compose/ProfileScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -15,8 +16,10 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -25,6 +28,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
@@ -44,6 +48,9 @@ fun ProfileScreen(){
         TopBar(name = "Aamir Ashraf")
         Spacer(modifier = Modifier.height(10.dp))
         ProfileSection()
+        Spacer(modifier = Modifier.height(25.dp))
+        ButtonSection(modifier = Modifier.fillMaxWidth())
+        Spacer(modifier = Modifier.height(25.dp))
     }
 }
 @Composable
@@ -225,6 +232,80 @@ fun ProfileDescription(
 
     }
 
+}
+@Composable
+fun ButtonSection(
+    modifier: Modifier=Modifier
+){
+    val minimumWidth=75.dp
+    val minimumHeight=30.dp
+    Row (
+        horizontalArrangement = Arrangement.SpaceEvenly,
+        modifier = modifier
+    ){
+        ActionButton(
+            text = "Following",
+            icon = Icons.Default.KeyboardArrowDown,
+            modifier = Modifier
+                .defaultMinSize(minimumWidth)
+                .height(minimumHeight)
+        )
+        ActionButton(
+            text = "Message",
+
+            modifier = Modifier
+                .defaultMinSize(minimumWidth)
+                .height(minimumHeight)
+        )
+        ActionButton(
+            text = "Email",
+           
+            modifier = Modifier
+                .defaultMinSize(minimumWidth)
+                .height(minimumHeight)
+        )
+        ActionButton(
+
+            icon = Icons.Default.KeyboardArrowDown,
+            modifier = Modifier
+                .defaultMinSize(minimumWidth)
+                .height(minimumHeight)
+        )
+    }
+}
+
+//now lets create reusable composable for button
+@Composable
+fun ActionButton(
+    modifier: Modifier=Modifier,
+    text:String?=null,
+    icon:ImageVector?=null
+){
+    Row (
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier
+            .border(
+                width = 1.dp,
+                color = Color.LightGray,
+                shape = RoundedCornerShape(5.dp)
+            )
+            .padding(6.dp)
+    ){
+        if (text!=null){
+            Text(
+                text = text,
+                fontWeight = FontWeight.SemiBold,
+                fontSize = 14.sp
+            )
+        }
+        if(icon!=null){
+            Icon(imageVector = icon,
+                contentDescription = null,
+                tint = Color.Black
+            )
+        }
+    }
 }
 //@Composable
 //fun Aamir(
